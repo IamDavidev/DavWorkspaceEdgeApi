350ec95a086c14c18f13b89249607cae9379285b
IamDavidev <davidlez2mgk@gmail.com>
2023-08-15 22:20:27 -0600

    [ ` Introduced new features. · ✨ ` ] function upload image with oak

--- supabase/functions/upload-image/index.ts
+++ supabase/functions/upload-image/index.ts
@@ -2,24 +2,33 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { Application, Router } from '~oak/mod.ts'
 
-console.log("Hello from Functions!")
+const DEFAULT_PATH = '/upload-image'
+const app = new Application()
+const router = new Router()
 
-serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
+// /gretting -> greeting
+export function genPath(path: string): string {
+  const parsePath = path.replace('/', '')
+  return `${DEFAULT_PATH}/${parsePath}`
+}
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+router
+  .get(genPath('/'), ctx => {
+    ctx.response.body = 'Hello World'
+  })
+  .post(genPath('/greet'), async ctx => {
+    const result = ctx.request.body({
+      type: 'json'
+    })
+    const body = await result.value
+    const name = body.name
 
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+    ctx.response.body = `Hello ${name}`
+  })
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen({ port: 8000 })
